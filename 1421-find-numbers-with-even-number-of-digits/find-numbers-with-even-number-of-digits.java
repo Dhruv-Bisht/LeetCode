94873380c9b0aa0a1d5f4e55b641091e314bae80
Dhruv-Bisht <103594018+Dhruv-Bisht@users.noreply.github.com>
2026-09-11 12:02:49 +0530

    Time: 1 ms (99.48%) | Memory: 44.5 MB (62.90%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        // int count = 0;
+        // int result = 0;
+        // int len = nums.length;
+        // for(int i=0; i<len; i++){
+        //     int n = nums[i];
+        //     int count = 0;
+        //     while(n>0)
+        //     {   
+        //         n = n / 10;
+        //         count ++;
+        //     }
+        //     if(count % 2 == 0){
+        //         result ++;
+        //     }
+        // }
+        // return result;
+
+        // In Java:- when number is Log10(num) gives us how many times a number is divided before becoming 0;
+        int result = 0;
+        for(int num:nums){
+            int digit = (int) Math.log10(num) + 1;
+            if(digit %2 == 0){
+                result++;
+            }
+        }
+        return result;
+    }
+}
