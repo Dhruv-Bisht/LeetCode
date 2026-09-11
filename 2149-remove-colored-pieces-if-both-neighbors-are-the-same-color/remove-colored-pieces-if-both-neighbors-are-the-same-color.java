9e629634ffe13d553bdd60d2710cfd67615a75c0
Dhruv-Bisht <103594018+Dhruv-Bisht@users.noreply.github.com>
2026-09-11 13:08:16 +0530

    Time: 7 ms (100.00%) | Memory: 47.6 MB (55.28%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,19 +1,34 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
-        int aliceMoves = 0;
-        int bobMoves = 0;
-        for(int i=1; i<colors.length()-1; i++){
-            char current = colors.charAt(i);
-            char left = colors.charAt(i-1);
-            char right = colors.charAt(i+1);
+        // int aliceMoves = 0;
+        // int bobMoves = 0;
+        // for(int i=1; i<colors.length()-1; i++){
+        //     char current = colors.charAt(i);
+        //     char left = colors.charAt(i-1);
+        //     char right = colors.charAt(i+1);
 
-            if(current == 'A' && left == 'A' && right == 'A'){
-                aliceMoves ++;
-            }
-            else if(current == 'B' && left == 'B' && right == 'B'){
-                bobMoves ++;
+        //     if(current == 'A' && left == 'A' && right == 'A'){
+        //         aliceMoves ++;
+        //     }
+        //     else if(current == 'B' && left == 'B' && right == 'B'){
+        //         bobMoves ++;
+        //     }
+        // }
+        // return aliceMoves > bobMoves;
+
+        char[] ch = colors.toCharArray();
+        int n = ch.length;
+        int alice = 0, bob = 0;
+        for(int i=1; i<n-1; i++){
+            if(ch[i] == ch[i+1] && ch[i] == ch[i-1]){
+                if(ch[i] == 'A'){
+                    alice++;
+                }
+                else{
+                    bob++;
+                }
             }
         }
-        return aliceMoves > bobMoves;
+        return alice>bob;
     }
 }
