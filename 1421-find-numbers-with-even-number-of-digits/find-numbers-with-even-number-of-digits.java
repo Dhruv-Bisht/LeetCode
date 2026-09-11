3ec4bdddede441dbdf5410b6e40cf074be054a62
Dhruv-Bisht <103594018+Dhruv-Bisht@users.noreply.github.com>
2026-09-11 12:03:28 +0530

    Time: 1 ms (99.48%) | Memory: 44.7 MB (47.28%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,5 +1,14 @@
 class Solution {
     public int findNumbers(int[] nums) {
+        // In Java:- when number is Log10(num) gives us how many times a number is divided before becoming 0;
+        int result = 0;
+        for(int num:nums){
+            int digit = (int) Math.log10(num) + 1;
+            if(digit %2 == 0){
+                result++;
+            }
+        }
+        return result;
         // int count = 0;
         // int result = 0;
         // int len = nums.length;
@@ -16,15 +25,5 @@ class Solution {
         //     }
         // }
         // return result;
-
-        // In Java:- when number is Log10(num) gives us how many times a number is divided before becoming 0;
-        int result = 0;
-        for(int num:nums){
-            int digit = (int) Math.log10(num) + 1;
-            if(digit %2 == 0){
-                result++;
-            }
-        }
-        return result;
     }
 }
